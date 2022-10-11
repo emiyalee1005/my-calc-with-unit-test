cfbcfd47b09b0cebeea62abb2a9817fa74109566
EmiyaLee <2288136aaa@gmail.com>
2022-10-11 22:03:31 +0800

    REFINE CODE

--- src/components/__tests__/MyCalc.spec.ts
+++ src/components/__tests__/MyCalc.spec.ts
@@ -12,46 +12,44 @@ describe("MyCalc", () => {
     const wrapper = mount(MyCalc, { props: {} });
     const domMap: Record<string, HTMLButtonElement> = {};
 
+    // check num btn
     for (let k = 0; k < 10; ++k) {
       const targetDom = wrapper.element.querySelector(`#num-${k}`);
       if (!targetDom) throw new Error(`NUM ${k} IS NOT FOUND`);
       domMap[k] = targetDom as HTMLButtonElement;
     }
 
+    // check plus btn
     const plusDom = wrapper.element.querySelector("#btn-plus");
     if (!plusDom) throw new Error(`PLUS IS NOT FOUND`);
     domMap["+"] = plusDom as HTMLButtonElement;
 
+    // check minus btn
     const minusDom = wrapper.element.querySelector("#btn-minus");
     if (!minusDom) throw new Error(`MINUS IS NOT FOUND`);
     domMap["-"] = minusDom as HTMLButtonElement;
 
+    // check del btn
     const delDom = wrapper.element.querySelector("#btn-del");
     if (!delDom) throw new Error(`DEL IS NOT FOUND`);
     domMap["del"] = delDom as HTMLButtonElement;
 
+    // input expr
     domMap["1"].click();
     domMap["+"].click();
     domMap["1"].click();
     domMap["-"].click();
     domMap["1"].click();
 
+    // wait vue to render new changed content asynchronously
     await sleep();
 
+    // check displayed expr
     const exprDom = wrapper.element.querySelector(".expr");
     expect(exprDom?.innerHTML).toEqual("1+1-1");
 
+    // check final result
     const resultNumberDom = wrapper.element.querySelector(".result-number");
     expect(resultNumberDom?.innerHTML).toEqual("1");
-
-    // const testDom = wrapper.element.querySelector(".test");
-    // if (testDom) {
-    //   testDom.innerHTML = "TEST";
-    //   expect(testDom.innerHTML).toContain("TEST");
-    // } else {
-    //   throw new Error("TEST DOM IS NOT FOUND");
-    // }
-    //
-    // expect(wrapper.text()).toContain("Hello Vitest");
   });
 });
